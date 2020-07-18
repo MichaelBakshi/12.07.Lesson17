1b364365f7c233b88275666d91431aadc5a89e39
MichaelBakshi <62958898+MichaelBakshi@users.noreply.github.com>
2020-07-18 21:32:52 +0300

    Add files via upload

---
+++ Lesson 17/Lesson 17/Citizen.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_17
+{
+    class Citizen
+    {
+        public string _name;
+        public Citizen[] children;
+        public readonly int _fatherID;
+        public readonly int _id;
+        public static int _numberOfCurrentCitizens;
+        public const int _maximunNumberOfCitizens = 0;
+
+        public Citizen(string _name, int _fatherID, int _id)
+        {
+            this._name = _name;
+            this._fatherID = _fatherID;
+            this._id = _id;
+        }
+
+        public static void PrintNumberOfCitizens()
+        {
+            Console.WriteLine(_numberOfCurrentCitizens);
+        }
+
+        public static bool ReachedHalfOfMaximumSize()
+        {
+            if (_numberOfCurrentCitizens >= _maximunNumberOfCitizens / 2)
+                return true;
+            else
+                return false;
+        }
+
+        public void PrintID()
+        {
+            Console.WriteLine(_id);
+        }
+
+        public void PrintGapBetweenMyIDAndFather()
+        {
+            Console.WriteLine(Math.Abs(_fatherID - _id));
+        }
+
+        public void SetChildren(Citizen[] citizens)
+        {
+            children = citizens;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+
+
+
+
+       
+    }
+}
---
+++ Lesson 17/Lesson 17/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Citizen father = new Citizen("Father", 11111, 11112);
+
+            Citizen firstChild = new Citizen("Avraham", 11112, 11113);
+            Citizen secondChild = new Citizen("Yitzhak", 11112, 11114);
+            Citizen thirdChild = new Citizen("Yaakov", 11112, 11115);
+
+            Citizen[] childrenOfFather = new Citizen[3];
+            childrenOfFather[0] = firstChild;
+            childrenOfFather[1] = secondChild;
+            childrenOfFather[2] = thirdChild;
+            father.SetChildren(childrenOfFather);
+
+            Console.WriteLine(HasChildren(father));
+            Console.WriteLine(CheckValidity(father));
+        }
+
+         static bool HasChildren(Citizen citizen)
+        {
+            return citizen.children != null && citizen.children.Length > 0;
+        }
+
+         static bool CheckValidity (Citizen citizen)
+        {
+            if (HasChildren(citizen)) 
+            {
+             
+                for (int i = 0; i < citizen.children.Length; i++)
+                {                    
+                    if (citizen.children[i]._id != citizen.children[i]._fatherID)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            else
+            return false;
+        }
+    }
+}
